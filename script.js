8478112867dd870124cffcd02dd7f6fea85698d2
ArturT <mr.arturio@gmail.com>
2023-10-26 22:34:33 -0400

    add Function to update slider position when a number is typed into an input field

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function updateMonthlyPayment() {
   document.getElementById("monthly-payment").value = monthlyPayment.toFixed(2);
 }
 
+// Function to update slider position when a number is typed into an input field
+function updateSliderFromInput(inputId, sliderId) {
+  const inputValue = parseInt(document.getElementById(inputId).value);
+  document.getElementById(sliderId).value = inputValue;
+  updateMonthlyPayment();
+}
+
 // Attach event listeners to sliders
 document.getElementById("cost-slider").addEventListener("input", function () {
   document.getElementById("treatment-cost").value = this.value;
@@ -25,6 +32,19 @@ document.getElementById("payment-slider").addEventListener("input", function () 
 document.getElementById("term-slider").addEventListener("input", function () {
   document.getElementById("term-length").value = this.value;
   updateMonthlyPayment();
+});
+
+// Attach event listeners to input fields for real-time slider update
+document.getElementById("treatment-cost").addEventListener("input", function () {
+  updateSliderFromInput("treatment-cost", "cost-slider");
+});
+
+document.getElementById("initial-payment").addEventListener("input", function () {
+  updateSliderFromInput("initial-payment", "payment-slider");
+});
+
+document.getElementById("term-length").addEventListener("input", function () {
+  updateSliderFromInput("term-length", "term-slider");
 });
 
 // Initial calculation
