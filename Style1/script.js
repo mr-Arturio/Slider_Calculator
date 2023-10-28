3729ca6bf640eba4c25c92673e375f5deaeac003
ArturT <mr.arturio@gmail.com>
2023-10-27 22:26:53 -0400

    jQuery script

--- Style1/script.js
+++ Style1/script.js
@@ -1,5 +1,22 @@
 $(function () {
-  // Initialize Treatment Cost Slider
+
+// Initialize Treatment Cost Slider
+$("#slider9").slider({
+  range: "min", // You can set the range type as needed
+  value: 7995, // Initial value
+  min: 0,     // Minimum value
+  max: 10000, // Maximum value
+  slide: function (event, ui) {
+    // Update the input field with the selected value
+    $("#treatment-cost").val(ui.value);
+  },
+});
+
+// Set the initial value of the input field to match the slider
+$("#treatment-cost").val($("#slider9").slider("value"));
+
+  
+  // Initialize Initial Payment Slider
   $("#slider1").slider({
     range: "min",
     value: 2000,
