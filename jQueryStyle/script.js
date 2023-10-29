2749aff2f690b1b64360a6f3593752e406d8c896
ArturT <mr.arturio@gmail.com>
2023-10-28 22:31:48 -0400

    refactore, remove unused

--- jQueryStyle/script.js
+++ jQueryStyle/script.js
@@ -1,88 +1,99 @@
-var module_64429374222 = function() {
-  var __hs_messages = {};
-  i18n_getmessage = function() {
-      return hs_i18n_getMessage(__hs_messages, hsVars.language, arguments)
-  }
-  ,
-  i18n_getlanguage = function() {
-      return hsVars.language
-  }
-  ,
-  $(document).ready((function() {
-      function update() {
-          $interest = 0,
-          $treatmentCost = $("#treatment-cost").val(),
-          $amount1 = $("#amount").val(),
-          $dayscount = $("#days").val(),
-          $amount2 = parseInt($treatmentCost) - parseInt($amount1),
-          $apr = $amount2 / parseInt($dayscount),
-          $apr < 0 && ($apr = 0),
-          $("#amount2").val(parseFloat($apr).toFixed(2)),
-          $("#amount3").val(parseFloat($amount2 - $amount1).toFixed(2)),
-          $("#amount4").val($apr)
-      }
-      $("#slider9").slider({
-          max: 1e4,
-          min: 50,
-          step: 50,
-          slide: function(event, ui) {
-              $(this).parent().find(".trt-cost").val(ui.value),
-              $("#treatment-cost").val(ui.value),
-              update()
-          },
-          create: function(event, ui) {
-              $(this).slider("value", $(this).parent().find(".trt-cost").val())
+const SliderModule = (function () {
+  // Private variables
+  const __hs_messages = {};
+  let $interest = 0;
+
+  // Private functions
+  const update = () => {
+    const $treatmentCost = $("#treatment-cost").val();
+    const $amount1 = $("#amount").val();
+    const $dayscount = $("#days").val();
+    const $amount2 = parseInt($treatmentCost) - parseInt($amount1);
+    let $apr = $amount2 / parseInt($dayscount);
+
+    if ($apr < 0) {
+      $apr = 0;
+    }
+
+    $("#amount2").val(parseFloat($apr).toFixed(2));
+    $("#amount4").val($apr);
+  };
+
+  const initSlider = (sliderSelector, options) => {
+    $(sliderSelector).slider(options);
+  };
+
+  const createSlider = (sliderSelector, inputSelector) => {
+    initSlider(sliderSelector, {
+      slide: function (event, ui) {
+        $(this).parent().find(inputSelector).val(ui.value);
+        update();
+      },
+      create: function (event, ui) {
+        $(this).slider("value", $(this).parent().find(inputSelector).val());
+      },
+    });
+  };
+
+  // Public function for initializing the slider module
+  const initialize = () => {
+    $(document).ready(function () {
+      // Initialize sliders and set their options
+      initSlider("#slider9", {
+        max: 10000,
+        min: 50,
+        step: 50,
+      });
+      createSlider("#slider9", ".trt-cost");
+
+      initSlider("#slider1", {
+        max: 7995,
+        min: 0,
+        step: 50,
+      });
+      createSlider("#slider1", ".downpayment");
+
+      initSlider("#slider2", {
+        max: 36,
+        min: 1,
+        step: 1,
+      });
+      createSlider("#slider2", ".terms");
+
+      $("#days").val($("#slider2").slider("value"));
+
+      $("#days").change(function (event) {
+        const data = $("#days").val();
+        if (data.length > 0) {
+          if (parseInt(data) >= 0 && parseInt(data) <= 31) {
+            $("#slider2").slider("option", "value", data);
+          } else {
+            if (parseInt(data) < 1) {
+              $("#days").val("1");
+              $("#slider2").slider("option", "value", "1");
+            }
+            if (parseInt(data) > 31) {
+              $("#days").val("31");
+              $("#slider2").slider("option", "value", "31");
+            }
           }
-      }),
-      $("#slider1").slider({
-          max: 7995,
-          min: 0,
-          step: 50,
-          slide: function(event, ui) {
-              $(this).parent().find(".downpayment").val(ui.value),
-              $("#amount").val(ui.value),
-              update()
-          },
-          create: function(event, ui) {
-              $(this).slider("value", $(this).parent().find(".downpayment").val())
-          }
-      }),
-      $("#slider2").slider({
-          max: 36,
-          min: 1,
-          step: 1,
-          slide: function(event, ui) {
-              $(this).parent().find(".terms").val(ui.value),
-              $("#days").val(ui.value),
-              update()
-          },
-          create: function(event, ui) {
-              $(this).slider("value", $(this).parent().find(".terms").val())
-          }
-      }),
-      $("#days").val($("#slider2").slider("value")),
-      $("#days").change((function(event) {
-          var data = $("#days").val();
-          data.length > 0 ? parseInt(data) >= 0 && parseInt(data) <= 31 ? $("#slider2").slider("option", "value", data) : (parseInt(data) < 1 && ($("#days").val("1"),
-          $("#slider2").slider("option", "value", "1")),
-          parseInt(data) > 31 && ($("#days").val("31"),
-          $("#slider2").slider("option", "value", "31"))) : $("#slider2").slider("option", "value", "1"),
-          $("#date").text(function(days) {
-              var mths = new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
-                , d = new Date;
-              d.setHours(d.getHours() + 24 * days);
-              var currD = d.getDate()
-                , currM = d.getMonth()
-                , currY = d.getFullYear();
-              return mths[currM] + " " + currD + ", " + currY
-          }(parseInt($("#days").val())))
-      }
-      )),
-      update(),
-      $("#treatment-cost").on("change paste keyup", (function() {
-          update()
-      }
-      ))
-  }
-  ))
-}();
+        } else {
+          $("#slider2").slider("option", "value", "1");
+        }
+
+             });
+
+      // Initialize and handle changes for the treatment cost input field
+      update();
+      $("#treatment-cost").on("change paste keyup", function () {
+        update();
+      });
+    });
+  };
+
+  // Public methods
+  return {
+    initialize: initialize,
+  };
+})();
+SliderModule.initialize();
