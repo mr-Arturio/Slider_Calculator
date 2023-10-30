29a2b84eaa3a017fd0fe8c6791e1642198f9c314
ArturT <mr.arturio@gmail.com>
2023-10-29 22:14:07 -0400

    no negative numbers, fix munths legth and othe user inputs

--- Basic/script.js
+++ Basic/script.js
@@ -1,11 +1,18 @@
 // Function to update the monthly payment
 function updateMonthlyPayment() {
-  const treatmentCost = parseInt(document.getElementById("treatment-cost").value);
-  const initialPayment = parseInt(document.getElementById("initial-payment").value);
+  const treatmentCost = parseInt(
+    document.getElementById("treatment-cost").value
+  );
+  const initialPayment = parseInt(
+    document.getElementById("initial-payment").value
+  );
   const termLength = parseInt(document.getElementById("term-length").value);
 
   // Calculate the monthly payment
-  const monthlyPayment = (treatmentCost - initialPayment) / termLength;
+  let monthlyPayment = (treatmentCost - initialPayment) / termLength;
+
+  // Ensure that the monthlyPayment is never negative
+  monthlyPayment = Math.max(0, monthlyPayment);
 
   // Display the result
   document.getElementById("monthly-payment").value = monthlyPayment.toFixed(2);
@@ -13,14 +20,33 @@ function updateMonthlyPayment() {
 
 // Function to update slider position when a number is typed into an input field
 function updateSliderFromInput(inputId, sliderId) {
-  const inputValue = parseInt(document.getElementById(inputId).value);
+  const inputElement = document.getElementById(inputId);
+  const inputValue = parseInt(inputElement.value);
   const slider = document.getElementById(sliderId);
+  const max = parseInt(inputElement.getAttribute("max"));
 
-  if (inputId === "initial-payment") {
-    const treatmentCost = parseInt(document.getElementById("treatment-cost").value);
+  if (inputId === "term-length") {
+    // Check if the input value exceeds the maximum specified by the term-slider
+    const termSliderMax = parseInt(
+      document.getElementById("term-slider").getAttribute("max")
+    );
+    if (inputValue > termSliderMax) {
+      inputElement.value = termSliderMax;
+      slider.value = termSliderMax;
+    } else {
+      slider.value = inputValue;
+    }
+  }
+
+  if (inputValue > max) {
+    inputElement.value = max;
+  } else if (inputId === "initial-payment") {
+    const treatmentCost = parseInt(
+      document.getElementById("treatment-cost").value
+    );
     if (inputValue > treatmentCost) {
       // Prevent "Initial Payment" from being more than "Treatment Cost"
-      document.getElementById(inputId).value = treatmentCost;
+      inputElement.value = treatmentCost;
       slider.value = treatmentCost;
     } else {
       slider.value = inputValue;
@@ -41,10 +67,12 @@ document.getElementById("cost-slider").addEventListener("input", function () {
   updateMonthlyPayment();
 });
 
-document.getElementById("payment-slider").addEventListener("input", function () {
-  document.getElementById("initial-payment").value = this.value;
-  updateSliderFromInput("initial-payment", "payment-slider");
-});
+document
+  .getElementById("payment-slider")
+  .addEventListener("input", function () {
+    document.getElementById("initial-payment").value = this.value;
+    updateSliderFromInput("initial-payment", "payment-slider");
+  });
 
 document.getElementById("term-slider").addEventListener("input", function () {
   document.getElementById("term-length").value = this.value;
@@ -52,13 +80,17 @@ document.getElementById("term-slider").addEventListener("input", function () {
 });
 
 // Attach event listeners to input fields for real-time slider update
-document.getElementById("treatment-cost").addEventListener("input", function () {
-  updateSliderFromInput("treatment-cost", "cost-slider");
-});
+document
+  .getElementById("treatment-cost")
+  .addEventListener("input", function () {
+    updateSliderFromInput("treatment-cost", "cost-slider");
+  });
 
-document.getElementById("initial-payment").addEventListener("input", function () {
-  updateSliderFromInput("initial-payment", "payment-slider");
-});
+document
+  .getElementById("initial-payment")
+  .addEventListener("input", function () {
+    updateSliderFromInput("initial-payment", "payment-slider");
+  });
 
 document.getElementById("term-length").addEventListener("input", function () {
   updateSliderFromInput("term-length", "term-slider");
